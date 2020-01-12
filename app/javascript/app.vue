7a11599f984335c977083064086228466aebedb2
kickbuttowski80 <pazpaz25@gmail.com>
2020-01-11 18:21:08 -0800

    running vue js successfuly

---
+++ app/javascript/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <button v-on:click="addOne">Add One</button>
+    {{counter}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello Vue!",
+      counter: 0
+    }
+  },
+  methods:{
+    addOne: function(){ 
+          alert(this.message)
+          this.counter += 1;
+         
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
