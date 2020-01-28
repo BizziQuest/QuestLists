9394ed2bfdbc90133e85355e2f9239981e65696b
kickbuttowski80 <pazpaz25@gmail.com>
2020-01-28 14:34:07 -0800

    added vuetify css to hello_vue js as an import and now css for vuetify works well

---
+++ app/javascript/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <p> {{message}} </p>
+    <v-btn class="primary" v-on:click="messageIs()"> what is message? </v-btn>
+    <v-alert
+      :value = "messageValue"
+      type = "success"
+    >
+      This is a success alert.
+    </v-alert>
+   
+  </v-app>
+</template>
+
+
+<script>
+
+export default {
+  components:{
+  
+  },
+  data(){
+    return {
+        messageValue: false,
+        message: "Vuetify is here"
+    }  
+  },
+  methods:{
+    messageIs(){
+       this.messageValue == true ? this.messageValue =false : this.messageValue =true
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
