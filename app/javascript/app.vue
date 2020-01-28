887901b1d9d89493e9c61e81c3e8f99f4bc0f1aa
Samuel C Tyler <skamansam@gmail.com>
2020-01-28 10:44:59 -0500

    copt repo from KickButtowski80/rails-vue-vuetify

---
+++ app/javascript/app.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <p> {{message}} </p>
+    <v-btn class="primary" v-on:click="messageIs()"> what is message? </v-btn>
+    <v-alert
+      :value = "messageValue"
+      type = "success"
+    >
+      This is a success alert.
+    </v-alert>
+    <google-contacts></google-contacts>
+    <sample-vuetify> </sample-vuetify>
+  </v-app>
+</template>
+
+
+<script>
+import GoogleContacts from './components/GoogleContacts'
+import sampleVuetify from './components/sampleVuetify'
+export default {
+  components:{
+    'google-contacts': GoogleContacts,
+    'sample-vuetify': sampleVuetify
+  },
+  data(){
+    return {
+        messageValue: false,
+        message: "Vuetify is here"
+    }  
+  },
+  methods:{
+    messageIs(){
+       this.messageValue == true ? this.messageValue =false : this.messageValue =true
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
